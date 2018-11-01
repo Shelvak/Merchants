60adb6f4ec9f0edc1325d3c679d4f8d09807ab7f
Néstor Coppi <nestorcoppi@gmail.com>
2018-10-31 21:20:40 -0300

    Prevent negative values

--- app/assets/javascripts/generals.js
+++ app/assets/javascripts/generals.js
@@ -1,31 +1,31 @@
 jQuery(function(){
 //    Autocalculate the finalprice
         $('#pricedist,#iva,#earn').keyup(function() {
-          $('#price').attr('value',$('#pricedist').val() * 
-            (parseFloat($('#iva').val())/100 + 1) * 
-            (parseFloat($('#earn').val())/100 + 1) ); 
+          $('#price').attr('value',$('#pricedist').val() *
+            (parseFloat($('#iva').val())/100 + 1) *
+            (parseFloat($('#earn').val())/100 + 1) );
         });
-        
+
         //   Delete the "snowman" from search and disable the button
         $('form').submit(function() {
            $(this).find('input[type="submit"],\n\
                 input[name="utf8"]').attr('disabled', true);
         });
-           
+
 //      Calcule the new stock
-        var stock = parseFloat($('#stock').val());
-        var original = parseFloat($('#stock').val());
+        var originalStock = parseFloat($('#stock').val());
         $('#newstock').on('change', function() {
-       
-           if ($('#newstock').val() == "" ){ 
-               $('#stock').attr('value', original);
-            }else {
-                stock += parseFloat( $('#newstock').val() )
-                $('#stock').attr('value', stock); 
-                stock = original;}
-                
+           if ($('#newstock').val() == "" ){
+             $('#stock').attr('value', originalStock);
+           } else {
+             newStock = parseFloat( $('#newstock').val() )
+             if (newStock >= 0) {
+               $('#stock').attr('value', originalStock + newStock);
+             }
+           }
+
         });
-        
+
         $('.excel_with_month').on('click', function() {
           month = prompt('Ingrese el numero del mes');
           year = prompt('Ingrese el numero del año YYYY');
@@ -33,6 +33,6 @@ jQuery(function(){
           url = window.location.pathname + ".csv?year=" + year + "&month=" + month;
           window.location = url;
         });
-        
-    
+
+
 });
