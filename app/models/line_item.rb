c580026281ad276f2dd8a9efba91a17844301613
Néstor Coppi <nestorcoppi@gmail.com>
2018-11-05 18:46:53 -0300

    More columns to csv

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -12,17 +12,19 @@ class LineItem < ActiveRecord::Base
 
   def self.to_csv
     CSV.generate(col_sep: ';') do |csv|
-      csv << ['Producto', 'Marca', 'Precio del producto',  'Cantidad vendida', 'Unidad del producto']
+      csv << ['Producto', 'Marca', 'Caracteristica', 'Cantidad', 'Unidad', 'Precio',  'Cantidad vendida']
 
       scoped.includes(:product).group_by(&:product).map do |product, collection|
         [
           product.name,
           product.mark,
+          product.fragance,
+          product.count,
+          product.uni,
           "$#{product.price.round(2)}",
-          collection.sum(&:quantity).to_f.round(2),
-          product.uni
+          collection.sum(&:quantity).to_f.round(2)
         ]
-      end.sort_by { |row| row[3] }.reverse.each do |row|
+      end.sort_by { |row| row.last }.reverse.each do |row|
         csv << row
       end
     end
